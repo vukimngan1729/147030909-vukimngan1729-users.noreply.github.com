ea46d24c74c301359d43283daac7ac859b08b436
Nam G VU <namgivu@gmail.com>
2023-10-17 13:13:56 +0700

    upd chambai

--- s02_chambai.py
+++ s02_chambai.py
@@ -3,11 +3,26 @@ def chambai(testcase, bailam_f):
   o_TESTCASE = testcase.get('output')
 
   assert callable(bailam_f), 'MUST HAVE bailam_f() method to score :bailam ^^'
-  
+
   try:
+    '''
+    eg 
+      testcase['input']: {'name':'Mom'}
+    will run 
+      bailam_f(name='Mom')
+    '''
     o_BAILAM = bailam_f(**testcase['input'])  # aka input_DAPAN
   except:
-    o_BAILAM = None
+    try:
+      '''
+      eg 
+        testcase['input']: [1,2,3]
+      will run 
+        bailam_f(1,2,3)
+      '''
+      o_BAILAM = bailam_f(*testcase['input'])  # aka input_DAPAN
+    except:
+      o_BAILAM = None
 
   score01 = int(o_BAILAM == o_TESTCASE)
   return score01, o_BAILAM
